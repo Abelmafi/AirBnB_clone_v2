260b8c7559254fc98c41b4cb01e6b71f248a7ed4
Abel <abelmesfin555@gmail.com>
2022-12-22 09:50:22 +0300

    output error fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,16 +24,27 @@ def do_deploy(archive_path):
         s_name = shlex.split(s_name)
         s_name = s_name[0]
 
+        # parse name
         put(archive_path, "/tmp/")
         new_path = "/data/web_static/releases/{}/".format(s_name)
         tmp_path = "/tmp/{}".format(name)
 
         run("sudo mkdir -p {}".format(new_path))
         run("sudo tar -xzf {} -C {}".format(tmp_path, new_path))
+
+        # remove archive
         run("sudo rm {}".format(tmp_path))
-        run("sudo mv {}web_static/* {}".format(new_path, new_path))
+        
+        # move contents in to host web_static
+        run('sudo rsync -a {}web_static/* {}'.format(new_path, new_path))
+        run ('sudo rm -rf {}web_static/*'.format(new_path))
+        # remove extranous web_static dir
         run("sudo rm -rf {}web_static".format(new_path))
+
+        # remove previous symbolic link
         run("sudo rm -rf /data/web_static/current")
+
+        # create new symbolic link
         run("sudo ln -s {} /data/web_static/current".format(new_path))
         print("New version deployed!")
         return True
