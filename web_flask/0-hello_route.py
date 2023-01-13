85f7a415a84310522e46004ee3df332d6dd9915b
Abel <abelmesfin555@gmail.com>
2023-01-13 11:23:00 +0300

    pycodestyle requirement error fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_hbnb():
-    return "Hello HBNB!";
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes=False
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
